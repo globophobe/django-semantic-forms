de4cbae083f1738c729bb60eccc9f26e06dcab9e
Alex <globophobe@gmail.com>
2026-03-29 11:26:14 +0900

    :necktie: format_html fixes

--- demo/demo_app/forms.py
+++ demo/demo_app/forms.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
 from semantic_forms import SemanticForm
 from semantic_forms.fields import (
@@ -34,64 +34,64 @@ class SemanticKitchenSinkForm(SemanticForm):
 
     char_field = SemanticCharField(
         label=_("Char"),
-        help_text=format_html(
+        help_text=mark_safe(
             "<pre><code>char_field = SemanticCharField()</code></pre>"
         ),
     )
     datetime_field = SemanticDateTimeField(
         label=_("Datetime"),
-        help_text=format_html(
+        help_text=mark_safe(
             "<pre><code>datetime_field = SemanticDateTimeField()</code></pre>"
         ),
     )
     date_field = SemanticDateField(
         label=_("Date"),
-        help_text=format_html(
+        help_text=mark_safe(
             "<pre><code>date_field = SemanticDateField()</code></pre>"
         ),
     )
     time_field = SemanticTimeField(
         label=_("Time"),
-        help_text=format_html(
+        help_text=mark_safe(
             "<pre><code>time_field = SemanticTimeField()</code></pre>"
         ),
     )
     textarea_field = SemanticTextareaField(
         label=_("Text"),
-        help_text=format_html(
+        help_text=mark_safe(
             "<pre><code>textarea_field = SemanticTextareaField()</code></pre>"
         ),
     )
     checkbox_field = SemanticCheckboxField(
         label=_("Checkbox"),
-        help_text=format_html(
+        help_text=mark_safe(
             "<pre><code>checkbox_field = SemanticCheckboxField()</code></pre>"
         ),
     )
     radio_field = SemanticRadioChoiceField(
         label=_("Radio"),
-        help_text=format_html(
+        help_text=mark_safe(
             "<pre><code>radio_field = SemanticRadioChoiceField()</code></pre>"
         ),
         choices=Colors.choices,
     )
     checkbox_select_field = SemanticCheckboxMultipleChoiceField(
         label=_("Checkbox select"),
-        help_text=format_html(
+        help_text=mark_safe(
             "<pre><code>checkbox_select_field = SemanticCheckboxMultipleChoiceField()</code></pre>"
         ),
         choices=Colors.choices,
     )
     select_field = SemanticChoiceField(
         label=_("Select"),
-        help_text=format_html(
+        help_text=mark_safe(
             "<pre><code>select_field = SemanticChoiceField()</code></pre>"
         ),
         choices=Colors.choices,
     )
     select_muliple_field = SemanticMultipleChoiceField(
         label=_("Select multiple"),
-        help_text=format_html(
+        help_text=mark_safe(
             "<pre><code>select_multiple_field = SemanticMultipleChoiceField()</code></pre>"
         ),
         choices=Colors.choices,
